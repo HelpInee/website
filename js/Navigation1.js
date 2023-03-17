0e88b6e6ca258a7d9b4e3c882f8be83fff59f33a
Anthony Hua <111655793+HelpInee@users.noreply.github.com>
2023-03-17 02:35:05 +0000

    Thu, Mar 16, 2023, 7:35 PM -07:00

--- js/Navigation1.js
+++ js/Navigation1.js
@@ -4,3 +4,8 @@ const container = document.querySelector('.container')
 
 open.addEventListener('click', () => {
     container.classList.add('show-nav')
+})
+
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav')
+})
