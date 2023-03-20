bacebe69b8b40baf2c92cc34421b8cfa2434ca8a
Anthony Hua <111655793+HelpInee@users.noreply.github.com>
2023-03-20 21:44:52 +0000

    Mon, Mar 20, 2023, 2:44 PM -07:00

--- js/jokes.js
+++ js/jokes.js
@@ -3,6 +3,11 @@ const jokeBtn = document.getElementByid('jokeBtn')
 
 jokeBtn.addEventListener('click', generateJoke)
 
-function generateJoke(){
-    
+async function generateJoke(){
+    const response = await featch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json'}
+    })
+
+    const data = await response.json()
+    jokeEl.innerHTML = data.joke
 }
